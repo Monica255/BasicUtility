62435230c8833ddf111c022d01f1ec54f6051b75
Monica <ms22469x@cimbniaga.co.id>
2026-05-21 13:22:07 +0700

    chore: add jitpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.basicutility"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.basicutility"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
